3d702a3b7af840f3797a2558ce2dadff6b07ed4d
Connor Lien <connorlien19@gmail.com>
2021-02-26 19:04:59 -0800

    added skeleton

---
+++ nextjs-forum/pages/api/Comment.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default class Comment extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>Comment</h1>;
+      </div>
+    );
+  }
+}
---
+++ nextjs-forum/pages/api/CommentTree.js
@@ -0,0 +1,8 @@
+import React from "react";
+import Comment from "./Comment";
+
+export default class CommentTree extends React.Component {
+  render() {
+    return <Comment />;
+  }
+}
