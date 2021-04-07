70a10aa1eaf6c28eb641ad3c44fd2ad2d3a684ac
William Zhang <williamyixin@berkeley.edu>
2021-04-06 21:15:15 -0700

    made some changes to backend

--- backend/main.go
+++ backend/main.go
@@ -5,15 +5,27 @@ import (
 	"context"
 	"firebase.google.com/go/v4"
 	"fmt"
+	"github.com/google/uuid"
 	"google.golang.org/api/iterator"
+	"google.golang.org/api/option"
 	"log"
 	"net/http"
-
-	"google.golang.org/api/option"
+	"time"
 )
 
 var ctx context.Context
 var client *firestore.Client
+
+type Comment struct {
+	Creator string
+	Path string
+	Time int64
+	Content string
+	Upvotes int
+	Downvotes int
+	Score int
+}
+
 
 func homePage(w http.ResponseWriter, r *http.Request){
 	fmt.Fprintf(w, "Welcome to the HomePage!")
@@ -21,28 +33,51 @@ func homePage(w http.ResponseWriter, r *http.Request){
 }
 
 func putTest(w http.ResponseWriter, r *http.Request) {
-	//_, err := client.Collection("topics").Doc("Interstellar").Set(ctx, map[string]interface{}{
-	//	"name": "Interstellar",
-	//	"releaseDate":  2014,
-	//})
-	//
-	//_ ,_ , err = client.Collection("topics").Doc("Interstellar").Collection("Comments").Add(ctx, map[string]interface{}{
-	//	"creator": "williamyixin",
-	//	"date": 2020,
-	//	"content": "wowowowowie i like",
-	//	"upvotes": 10000,
-	//	"downvotes": -1209,
-	//
+
+	uuid := uuid.NewString()
+	time := time.Now()
+	unixTime := time.Unix()
+
+	comment := Comment{
+		"williamyixin",
+		"Interstellar/qtg_sDwQMIerTiFmPaeLP_77-XqtKojZZrQYl2pbpNI/-enTTbkVvfarH8ybfSZ_Ow9aHib_YkJaEfmWt67IHVo",
+		unixTime,
+		"You actually see it happening also. There was a shot as they are landing looking down from the sky and you see something crash into the water.",
+		10000,
+		1209,
+		10000 - 1209,
+	}
+
+	ref := client.Collection("topics").Doc("Interstellar").
+		Collection("Comments").Doc("qtg_sDwQMIerTiFmPaeLP_77-XqtKojZZrQYl2pbpNI")
+
+	_, err := ref.Collection("Comments").Doc("-enTTbkVvfarH8ybfSZ_Ow9aHib_YkJaEfmWt67IHVo").
+		Collection("Comments").Doc(uuid).
+		Set(ctx, map[string]interface{}{
+			"creator": comment.Creator,
+			"path": comment.Path + "/" + uuid,
+			"time": comment.Time,
+			"content": comment.Content,
+			"upvotes": comment.Upvotes,
+			"downvotes": comment.Downvotes,
+			"score": comment.Score,
+		})
+
+
+	//_ , err := client.Collection("topics").Doc("Interstellar").
+	//	Collection("Comments").Doc("qtg_sDwQMIerTiFmPaeLP_77-XqtKojZZrQYl2pbpNI").
+	//	Collection("Comments").Doc("-enTTbkVvfarH8ybfSZ_Ow9aHib_YkJaEfmWt67IHVo").
+	//	Collection("Comments").Doc(uuid).
+	//	Set(ctx, map[string]interface{}{
+	//		"creator": comment.Creator,
+	//		"path": comment.Path,
+	//		"time": comment.Time,
+	//		"content": comment.Content,
+	//		"upvotes": comment.Upvotes,
+	//		"downvotes": comment.Downvotes,
+	//		"score": comment.Score,
 	//})
 
-	_ ,_ , err := client.Collection("topics").Doc("Interstellar").Collection("Comments").Doc("qtg_sDwQMIerTiFmPaeLP_77-XqtKojZZrQYl2pbpNI").Collection("Comments").Add(ctx, map[string]interface{}{
-		"creator": "williamyixin",
-		"date": 2020,
-		"content": "i don't like",
-		"upvotes": 10000,
-		"downvotes": -1209,
-
-	})
 	if err != nil {
 		log.Fatalf("Failed adding alovelace: %v", err)
 	}
@@ -58,7 +93,7 @@ func getTest(w http.ResponseWriter, r *http.Request) {
 		if err != nil {
 			log.Fatalf("Failed to iterate: %v", err)
 		}
-		fmt.Println(doc.Data())
+		fmt.Println(w,doc.Data())
 	}
 }
 
