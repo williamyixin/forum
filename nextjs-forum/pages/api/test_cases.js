046fab9f2511f55497843b09482433c9ac6082c0
William Zhang <williamyixin@berkeley.edu>
2021-04-06 21:15:15 -0700

    made some changes to backend

---
+++ nextjs-forum/pages/api/test_cases.js
@@ -0,0 +1,50 @@
+// POST Request input to insert a reply 
+
+let postRequest = {
+    "creator": "happybanditman",
+    "path": "Interstellar/qtg_sDwQMIerTiFmPaeLP_77-XqtKojZZrQYl2pbpNI/-enTTbkVvfarH8ybfSZ_Ow9aHib_YkJaEfmWt67IHVo",
+    "content": "What I disagree",
+  }
+
+
+// Output for GET Request to get tree under a thread 
+
+let getRequest = {
+    "comments": [
+        {
+            "creator": "happybanditman",
+            "path": "Interstellar/qtg_sDwQMIerTiFmPaeLP_77-XqtKojZZrQYl2pbpNI",
+            "upvotes": 2500,
+            "downvotes": 100,
+            "score": 2400,
+            "content": "That moment when they realize that the person who died on the water planet had probably only died minutes ago, that was something I had never considered until I heard them say it. That was a big oh shit moment for me",
+            "comments" :[
+                {
+                    "creator": "happybanditman",
+                    "path": "Interstellar/qtg_sDwQMIerTiFmPaeLP_77-XqtKojZZrQYl2pbpNI/-enTTbkVvfarH8ybfSZ_Ow9aHib_YkJaEfmWt67IHVo",
+                    "upvotes": 2500,
+                    "downvotes": 100,
+                    "score": 2400,
+                    "content": "You actually see it happening also. There was a shot as they are landing looking down from the sky and you see something crash into the water.",
+                    "comments" :[]
+                }
+            ]
+        }
+
+    ]
+}
+
+
+
+
+
+
+{
+    "creator": "happybanditman",
+    "path": "Interstellar/qtg_sDwQMIerTiFmPaeLP_77-XqtKojZZrQYl2pbpNI",
+    "upvotes": 2500,
+    "downvotes": 100,
+    "score": 2400,
+    "content": "That moment when they realize that the person who died on the water planet had probably only died minutes ago, that was something I had never considered until I heard them say it. That was a big oh shit moment for me",
+    "comments" :[]
+}
