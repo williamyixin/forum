71a49f9a1170a0215107facd6ddfe5cb3e1b3b6d
Colin Zhou <colinzhou@berkeley.edu>
2021-04-07 22:13:25 -0700

    updated title.js with test

--- nextjs-forum/pages/api/Title.js
+++ nextjs-forum/pages/api/Title.js
@@ -2,14 +2,23 @@ import React from "react";
 import Image from "next/image";
 import { Button, Row, Col, Form } from "react-bootstrap";
 
-export default class StartPost extends React.Component {
+export default class Title extends React.Component {
+  constructor(props) {
+    super(props);
+    this.title = {
+      "topic": "Interstellar",
+      "description": "In Earth's future, a global crop blight and second Dust Bowl are slowly rendering the planet uninhabitable. Professor Brand (Michael Caine), a brilliant NASA physicist, is working on plans to save mankind by transporting Earth's population to a new home via a wormhole. But first, Brand must send former NASA pilot Cooper (Matthew McConaughey) and a team of researchers through the wormhole and across the galaxy to find out which of three planets could be mankind's new home."
+    }
+  }
+
+
   render() {
     return (
       <div
         style={{
           display: "inline-block",
           maxWidth: "75vw",
-          height: "220px",
+          height: "230px",
           background: "white",
           marginTop: "30px",
           marginLeft: "40px",
@@ -18,10 +27,10 @@ export default class StartPost extends React.Component {
       >
         <div style={{marginLeft: "15px", marginTop: "15px"}}>
           <div style={{color: "#617A93", fontWeight: "bold",}}>
-            <h1>Movie Title Here: The Movie</h1>
+            <h1>{this.title.topic}</h1>
           </div>
           <div style={{color: "#5888B7",}}>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. </p>
+            <p>{this.title.description}</p>
           </div>
           <div style={{color: "#0B76E0"}}>
             <h4>121 comments</h4>
