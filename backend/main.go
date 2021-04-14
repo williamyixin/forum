b0ef25d694baa18f6bea8365df53b6cd75934eac
Michelle <mchen9129@berkeley.edu>
2021-04-14 03:20:57 -0700

    minor change, level index

--- backend/main.go
+++ backend/main.go
@@ -112,30 +112,12 @@ func getTest(w http.ResponseWriter, r *http.Request) {
 //	Score int
 //	Replies []Comment
 
-//func getReference(path string) *firestore.DocumentRef{
-//	path = "Interstellar/qtg_sDwQMIerTiFmPaeLP_77-XqtKojZZrQYl2pbpNI/-enTTbkVvfarH8ybfSZ_Ow9aHib_YkJaEfmWt67IHVo"
-//
-//	splittedArray := strings.Split(path, "/")
-//	ref := client.Collection("topics").Doc(splittedArray[0])
-//	for i := 1; i < len(splittedArray); i++ {
-//		 ref = ref.Collection("Comments").Doc(splittedArray[i])
-//	}
-//	testRef, _ := ref.Get(ctx)
-//	m := testRef.Data()
-//	fmt.Printf("Document data test: %#v\n", m)
-//	testRef2, _ := client.Collection("topics").Doc("Interstellar").
-//		Collection("Comments").Doc("qtg_sDwQMIerTiFmPaeLP_77-XqtKojZZrQYl2pbpNI").
-//		Collection("Comments").Doc("-enTTbkVvfarH8ybfSZ_Ow9aHib_YkJaEfmWt67IHVo").Get(ctx)
-//	m = testRef2.Data()
-//	fmt.Printf("Document data manual: %#v\n", m)
-//	return ref
-//}
-func makeTree(parent Comment, parString string, children []Comment) {
+func makeTree(parent Comment, parID string, children []Comment) {
 	for i, child := range children {
 		childSplit := strings.Split(child.Path, "/")
 		n := len(childSplit)
 
-		if strings.Compare(childSplit[n - 2], parString) == 0 {
+		if strings.Compare(childSplit[n - 2], parID) == 0 {
 			parent.Replies = append(parent.Replies, child)
 			makeTree(child, childSplit[n - 1], children[i:])
 		}
@@ -148,7 +130,7 @@ func getComments(topic string) []Comment {
 	commentDocs, _ := commentsIter.GetAll()
 	var comments []Comment
 	var ret []Comment
-	var replies []Comment
+	//var replies []Comment
 
 	for _, commentDoc := range commentDocs {
 		data := commentDoc.Data()
@@ -174,32 +156,38 @@ func getComments(topic string) []Comment {
 		return len(splitPathI) < len(splitPathJ)
 	})
 
-	for _, c := range comments {
+	level := 2
+	var indices []int
+
+	for i, c := range comments {
 		splitPath := strings.Split(c.Path, "/")
 		lenPath := len(splitPath)
 		if lenPath <= 2 {
 			ret = append(ret, c)
 		} else {
-			replies = append(replies, c)
-		}
-	}
-
-	var groupedReplies [][]Comment
-	for i, parent := range ret {
-		parString := parent.Path
-		groupedReplies = append(groupedReplies, make([]Comment, 0))
-
-		for _, reply := range replies {
-			if strings.Contains(reply.Path, parString) {
-				groupedReplies[i] = append(groupedReplies[i], reply)
+			if lenPath > level {
+				indices = append(indices, i)
+				level = lenPath
 			}
 		}
 	}
 
-	for i, p := range ret {
+	//var groupedReplies [][]Comment
+	//for i, parent := range ret {
+	//	parString := parent.Path
+	//	groupedReplies = append(groupedReplies, make([]Comment, 0))
+	//
+	//	for _, reply := range comments {
+	//		if strings.Contains(reply.Path, parString) {
+	//			groupedReplies[i] = append(groupedReplies[i], reply)
+	//		}
+	//	}
+	//}
+
+	for _, p := range ret {
 		pSplit := strings.Split(p.Path, "/")
 		n := len(pSplit)
-		makeTree(p, pSplit[n - 1], groupedReplies[i])
+		makeTree(p, pSplit[n - 1], comments[indices[0]:])
 	}
 
 	return ret
