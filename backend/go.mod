e54843eda67f9fe98b6965b42a42a715ed0a44ef
Michelle <mchen9129@berkeley.edu>
2021-04-13 23:22:49 -0700

    v1: implemented, may be broken

--- backend/go.mod
+++ backend/go.mod
@@ -2,4 +2,8 @@ module firebase.google.com/go
 
 go 1.15
 
-require firebase.google.com/go/v4 v4.2.0 // indirect
+require (
+	cloud.google.com/go/firestore v1.5.0 // indirect
+	firebase.google.com/go/v4 v4.4.0 // indirect
+	github.com/google/uuid v1.2.0 // indirect
+)
--- backend/main.go
+++ backend/main.go
@@ -10,6 +10,8 @@ import (
 	"google.golang.org/api/option"
 	"log"
 	"net/http"
+	"sort"
+	"strings"
 	"time"
 )
 
@@ -100,7 +102,108 @@ func getTest(w http.ResponseWriter, r *http.Request) {
 		fmt.Println(w,doc.Data())
 	}
 }
+//	Topic string
+//	Creator string
+//	Path string
+//	Time int64
+//	Content string
+//	Upvotes int
+//	Downvotes int
+//	Score int
+//	Replies []Comment
 
+//func getReference(path string) *firestore.DocumentRef{
+//	path = "Interstellar/qtg_sDwQMIerTiFmPaeLP_77-XqtKojZZrQYl2pbpNI/-enTTbkVvfarH8ybfSZ_Ow9aHib_YkJaEfmWt67IHVo"
+//
+//	splittedArray := strings.Split(path, "/")
+//	ref := client.Collection("topics").Doc(splittedArray[0])
+//	for i := 1; i < len(splittedArray); i++ {
+//		 ref = ref.Collection("Comments").Doc(splittedArray[i])
+//	}
+//	testRef, _ := ref.Get(ctx)
+//	m := testRef.Data()
+//	fmt.Printf("Document data test: %#v\n", m)
+//	testRef2, _ := client.Collection("topics").Doc("Interstellar").
+//		Collection("Comments").Doc("qtg_sDwQMIerTiFmPaeLP_77-XqtKojZZrQYl2pbpNI").
+//		Collection("Comments").Doc("-enTTbkVvfarH8ybfSZ_Ow9aHib_YkJaEfmWt67IHVo").Get(ctx)
+//	m = testRef2.Data()
+//	fmt.Printf("Document data manual: %#v\n", m)
+//	return ref
+//}
+func makeTree(parent Comment, parString string, children []Comment) {
+	for i, child := range children {
+		childSplit := strings.Split(child.Path, "/")
+		n := len(childSplit)
+
+		if strings.Compare(childSplit[n - 2], parString) == 0 {
+			parent.Replies = append(parent.Replies, child)
+			makeTree(child, childSplit[n - 1], children[i:])
+		}
+	}
+}
+
+func getComments(topic string) []Comment {
+	commentsIter := client.CollectionGroup("Comments").Where(
+		"topic", "==", "Interstellar").Documents(ctx)
+	commentDocs, _ := commentsIter.GetAll()
+	var comments []Comment
+	var ret []Comment
+	var replies []Comment
+
+	for _, commentDoc := range commentDocs {
+		data := commentDoc.Data()
+
+		newComment := Comment {
+			Creator: data["creator"].(string),
+			Path: data["path"].(string),
+			Time: data["date"].(int64),
+			Content: data["content"].(string),
+			Upvotes: data["upvotes"].(int),
+			Downvotes: data["downvotes"].(int),
+			Score: data["score"].(int),
+			Replies: make([]Comment, 0),
+		}
+
+		comments = append(comments, newComment)
+	}
+
+	sort.Slice(comments, func(i, j int) bool {
+		splitPathI := strings.Split(comments[i].Path, "/")
+		splitPathJ := strings.Split(comments[j].Path, "/")
+
+		return len(splitPathI) < len(splitPathJ)
+	})
+
+	for _, c := range comments {
+		splitPath := strings.Split(c.Path, "/")
+		lenPath := len(splitPath)
+		if lenPath <= 2 {
+			ret = append(ret, c)
+		} else {
+			replies = append(replies, c)
+		}
+	}
+
+	var groupedReplies [][]Comment
+	for i, parent := range ret {
+		parString := parent.Path
+		groupedReplies = append(groupedReplies, make([]Comment, 0))
+
+		for _, reply := range replies {
+			if strings.Contains(reply.Path, parString) {
+				groupedReplies[i] = append(groupedReplies[i], reply)
+			}
+		}
+	}
+
+	for i, p := range ret {
+		pSplit := strings.Split(p.Path, "/")
+		n := len(pSplit)
+		makeTree(p, pSplit[n - 1], groupedReplies[i])
+	}
+
+	return ret
+}
 
 
 func handleRequests() {
@@ -111,7 +214,7 @@ func handleRequests() {
 }
 
 func main() {
-	opt := option.WithCredentialsFile("./service_accounts/forum-7e971-firebase-adminsdk-e3jqe-520d6bf3c5.json")
+	opt := option.WithCredentialsFile("./service_accounts/key.json")
 	ctx = context.Background()
 	app, err := firebase.NewApp(ctx, nil, opt)
 	if err != nil {
